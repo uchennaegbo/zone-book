1e3e5fc8564321d7781747b8bdb6272e50663dba
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-12 01:02:26 +0100

    Authentication

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ZoneBook
                 </div>
 
                 <div class="links">
