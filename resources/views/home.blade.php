7742b5a49ccf7bb978beddb6c9fafb05b8626e8a
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-13 10:26:59 +0100

    Setup up views for frontend.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+<div class="container border">
+    <div class="row  justify-content-center">
+        <div class="col-2 p-5">
+    <img src="/svg/user.png" alt="A user" srcset="">
+        </div>
+            <div class="col-6">
+    <div class="pt-5">
+        <h1>Test User </h1>
+    </div>
+    <div class="d-flex">
+    <div class="pr-5"><strong>23</strong> books</div>
+    <div class="pr-5"><strong>553</strong> followers</div>
+    <div class="pr-5"><strong>100</strong> following</div></div>
         </div>
     </div>
 </div>
