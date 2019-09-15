11d101afc44cd28fa534a41caefdf318c534eb62
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-15 17:50:51 +0100

    Added edit and update function to a user's profile.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -11,8 +12,43 @@ class ProfilesController extends Controller
     {
         $user = User::findorFail($user);
 
-        return view('home', [
+        return view('profiles.index', [
             'user' => $user,
         ]);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update',$user->profile);
+
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $this->authorize('update',$user->profile);
+
+
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
+        ]);
+
+
+        if(request('image')) {
+            $imagePath = request('image')->store('profile','public');
+
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,1000);
+            $image->save();
+
+            auth()->user()->profile->update(array_merge(
+                $data,
+                ['image' => $imagePath]
+            ));
+        }
+
+        return redirect("/profile/{$user->id}");
     }
 }
