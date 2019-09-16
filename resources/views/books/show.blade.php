243edabc941de895491c9ef993cd6dafda3e57a8
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-09-16 02:25:55 +0100

    Displayed book title and description

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container pt-5 border">
+<div class="container pt-5 ">
   <div class="row">
 
     <div class="col-8 ">
@@ -9,17 +9,29 @@
     </div>
 
     <div class="col-4">
-      <div class="d-flex align-items-center">
-        <div class="pr-3">
-          <img src="/storage/{{$book->user->profile->image}}" class="w-100 rounded-circle" style="max-width:40px;">
-        </div>
+      <div>
+        <div class="d-flex align-items-center">
+          <div class="pr-3">
+            <img src="{{$book->user->profile->profileImage()}}" class="w-100 rounded-circle" style="max-width:40px;">
+          </div>
 
-        <div>
-          <div class="font-weight-bold">{{$book->user->username}}</div>
-          <!-- <p>{{$book->title}}</p>
-          <p>{{$book->description}}</p> -->
+          <div>
+            <div class="font-weight-bold"><a href="/profile/{{$book->user->id}}"><span
+                  class="text-dark">{{$book->user->username}}</span></a>
+              <a href="#" class="pl-3">Follow</a>
+            </div>
+          </div>
         </div>
+        <hr>
       </div>
+
+      <p><span class="font-weight-bold"><a href="/profile/{{$book->user->id}}"><span
+              class="text-dark">{{$book->user->username}}</span> </a>
+        </span>
+        {{$book->title}}
+      </p>
+
+      <p>{{$book->description}}</p>
 
     </div>
 
